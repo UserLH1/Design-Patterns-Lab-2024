d733796482368d796bbb50b91e9047bf20e8557c
UserLH1 <lazeahoratiu@gmail.com>
2024-10-18 09:03:27 +0300

    first commit

---
+++ src/main/java/ro/uvt/info/designpatternslab2024/DesignPatternsLab2024Application.java
@@ -0,0 +1,13 @@
+package ro.uvt.info.designpatternslab2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternsLab2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternsLab2024Application.class, args);
+    }
+
+}
